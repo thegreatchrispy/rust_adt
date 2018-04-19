89eef85332e3d9f20fdecf1a6b94e75375c35ddb
thegreatchrispy <thegreatchrispy@gmail.com>
2018-04-19 16:02:16 -0500

    Finished the f64 impl. Need to finish bag_exam & bag_test.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
 mod bag;
+mod bag_exam;
 use bag::Bag;
 
 fn main() {
-	let x = Bag::<f64>::new();
-	println!("x: {:?}", x);
+	let mut point_total = 0;
+	point_total += bag_exam::test1();
+	point_total += bag_exam::test2();
+	point_total += bag_exam::test3();
+	point_total += bag_exam::test4();
+	point_total += bag_exam::test5();
 
-	let y = Bag::<f64>::new_with_capacity(3);
-	println!("y: {:?}", y);
-
-	let mut z = Bag::<f64>::new_from_bag(&y);
-	println!("z: {:?}", z);
-
-	println!("z capacity: {}", z.get_capacity());
-
-	println!("z size: {}", z.size());
-
-	z.ensure_capacity(5);
+	println!("Total Points: {}", point_total);
 }
